cc7268c027e54f5b81c0a3c6b3a993668b170ec0
daothannhangian <binhk4a@ gmail.com>
2022-05-05 15:17:27 +0700

    JWT

---
+++ src/main/java/com/codegym/demospringboot/model/User.java
@@ -0,0 +1,23 @@
+package com.codegym.demospringboot.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+import static javax.persistence.FetchType.EAGER;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @ManyToMany(fetch = EAGER)
+    private Set<Role> roles;
+}
---
+++ src/main/java/com/codegym/demospringboot/service/role/IRoleService.java
@@ -0,0 +1,10 @@
+package com.codegym.demospringboot.service.role;
+
+import com.codegym.demospringboot.model.Role;
+import com.codegym.demospringboot.service.IGeneralService;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+public interface IRoleService extends IGeneralService<Role> {
+
+}
